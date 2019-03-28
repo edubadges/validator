d45ff3d9cbf0b4da0340972698f7142b4a73e990
smoquet <you@example.com>
2019-03-28 16:15:04 +0100

    fixes eduID uri format error

--- openbadges/verifier/verifier.py
+++ openbadges/verifier/verifier.py
@@ -156,6 +156,11 @@ def generate_report(store, options=DEFAULT_OPTIONS):
     report['errorCount'] = len([m for m in report['messages'] if m['messageLevel'] == MESSAGE_LEVEL_ERROR])
     report['warningCount'] = len([m for m in report['messages'] if m['messageLevel'] == MESSAGE_LEVEL_WARNING])
 
+    # make sure uri format check failure will pass
+    if report['errorCount'] == 1:
+        if 'not valid in unknown type node' in report['messages'][0]['result']:  # = uri format validity check fail
+            report['errorCount'] = 0
+
     is_valid = True  # Assume to be true at first
     if bool(report['errorCount']) or len(state.get('graph', [])) == 0:
         is_valid = False
